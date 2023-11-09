9f5bb69dd900528b7d710ea910d8c5b9ef7d18dd
jiayinkong <ry1n.kong@gmail.com>
2023-11-09 23:51:40 +0800

    feat: write a request from backend

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const data = ref([])
+
+onMounted(() => {
+  axios.get('http://localhost:3000/api/users')
+    .then(res => {
+      data.value = res.data
+    })
+})
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="user in data" :key="user.id">
+      {{ user.name }}
+    </li>
+  </ul>
+</template>
+
